bda8b2aa09c94c52611741eb5aaf109e16b6ef8c
John Doe <node@gmail.com>
2025-07-01 16:35:29 +0300

    fix: update base URL to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: '/techecon/',
+  base: '/technekon/',
   plugins: [vue()],
   resolve: {
     alias: {
