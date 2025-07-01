13081bfc21b6aa4c5191cbb49cd47c3891178ecd
John Doe <node@gmail.com>
2025-07-01 16:05:57 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   base: '/techecon/',
@@ -8,6 +8,22 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo: { name?: string }) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'assets/styles.[hash].css';
+          }
+          return 'assets/[name].[hash][extname]';
+        },
+      },
     },
   },
 });
