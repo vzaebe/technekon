e6f2e3ac5ac305a3e25dee308f25be53904a7606
John Doe <node@gmail.com>
2025-07-01 17:00:37 +0300

    neeeew

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: '/technekon/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': './src'
+      '@': '/src'
     }
   },
   css: {
